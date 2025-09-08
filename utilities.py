6d394b97f04ebe258f32d19118adbb8b0c0b31d2
shafe123 <ethan.h.shafer@gmail.com>
2025-09-08 10:55:15 -0400

    swap default name from clean to 'blank'

--- utilities.py
+++ utilities.py
@@ -9,7 +9,7 @@ def clear_code_blocks(input_notebook_path: str, output_notebook_path: str = ""):
             nb["cells"][index]["source"] = [] 
 
     if not output_notebook_path:
-        output_notebook_path = f"{input_notebook_path.replace('.ipynb', '')}_clean.ipynb"
+        output_notebook_path = f"{input_notebook_path.replace('.ipynb', '')}_blank.ipynb"
     with open(output_notebook_path, 'w') as out_file:
         dump(nb, out_file, indent=1)
 
