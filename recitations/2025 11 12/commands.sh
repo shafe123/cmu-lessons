6284bd832bf3f382970bae57d9ac5e6b5dac1998
Ethan Shafer <ethan.h.shafer@gmail.com>
2025-11-12 11:44:19 -0500

    working sed replace

--- recitations/2025 11 12/commands.sh
+++ recitations/2025 11 12/commands.sh
@@ -6,17 +6,21 @@ cd reviews
 # TODO 1 — List the names of all .txt files that contain the word "great" (case-insensitive)
 # Hint: Use grep with appropriate flags
 ############################################################
+grep -l -I -r great .
 
 ############################################################
 # TODO 2 — Count how many .txt files mention the word "programming" (case-insensitive)
 # Hint-1: You can use pipes to combine commands
 # Hint-2: We are counting files, not occurrences
 ############################################################
+grep -l -I -r programming . | wc -l
 
 ############################################################
 # TODO 3 — Replace all email addresses in .txt files with <EMAIL_REMOVED> in place and create a backup with .bak extension
 # Hint: Use sed with regex for a single file first
 ############################################################
+# sed -s -E -i.bak 's/[[:alnum:].]+@[[:alnum:]]+\.com/<EMAIL_REMOVED>/gI' reviews1.txt
+find *.txt | xargs sed -s -E -i.bak 's/[[:alnum:].]+@[[:alnum:]]+\.com/<EMAIL_REMOVED>/gI'
 
 ############################################################
 # TODO 4 — From books_metadata.csv, print only the Title and Price columns (space-separated)
@@ -30,3 +34,5 @@ cd reviews
 # Hint: Use awk to filter and count
 # Challenge: Do it by dynamically finding the column number based on the header row
 ############################################################
+
+cd ..
